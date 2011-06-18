5de2868ba73cab5cc2a8810e2bd34a6e2ab8731c
Georg Sauthoff <gsauthof@techfak.uni-bielefeld.de>
2011-06-18 21:43:48 +0200

    favicon, iptables

--- server/iptables.sh
+++ server/iptables.sh
@@ -3,12 +3,12 @@ set -e
 set -u
 
 
-UID=`id -u gapp`
+ID=`id -u gapp`
 
 iptables -N gapp_chain
-iptables -A gapp_chain -m owner --uid-owner $UID -p tcp --dport 1:1024 -j REJECT
-iptables -A gapp_chain -m owner --uid-owner $UID -p tcp -d 127.0.0.1 -j ACCEPT
-iptables -A gapp_chain -m owner --uid-owner $UID -j REJECT
+iptables -A gapp_chain -m owner --uid-owner $ID -p tcp --dport 1:1024 -j REJECT
+iptables -A gapp_chain -m owner --uid-owner $ID -p tcp -d 127.0.0.1 -j ACCEPT
+iptables -A gapp_chain -m owner --uid-owner $ID -j REJECT
 iptables -A OUTPUT -j gapp_chain
 
 
--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ make -f web/web.mf SRC=pages DST=/var/www/gap INSERT=web/repl
 cp -u pages/*_bib_bib.html /var/www/gap
 
 cp -u resources/gap.css /var/www/gap/resources
-cp -u resources/favicon.ico /var/www/gap/resources
+cp -u resources/favicon.ico /var/www/gap
 cp -u resources/header_noTitle.png /var/www/gap/resources
 
 mkdir -p /var/www/gap/grammar
